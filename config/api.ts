bf256be63a1cd436b307ce5ea775df95fa4b2ea2
MrNibo99 <153727578+MrNibo99@users.noreply.github.com>
2024-09-20 15:41:34 +0200

    Update api.ts

--- config/api.ts
+++ config/api.ts
@@ -2,7 +2,7 @@
 
 const apiUrl = process.env.NEXT_PUBLIC_DEV_MODE === '1' 
   ? "http://localhost:3000/api"  // Development API URL
-  : "https://www.aomworld.com/api";  // Production API URL
+  : "https://www.aomworld.pro/api";  // Production API URL
 
 export const apiData = {
     url: apiUrl,
