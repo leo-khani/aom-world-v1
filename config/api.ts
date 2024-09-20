90d02fa832fb5d16427c007ec6ca67abffc03aa2
MrNibo99 <153727578+MrNibo99@users.noreply.github.com>
2024-09-20 18:46:52 +0200

    Update api.ts

--- config/api.ts
+++ config/api.ts
@@ -1,15 +1,6 @@
-<<<<<<< HEAD
-const defaultApiUrl = "https://www.aomworld.pro/api";  // Default Production API URL
-const devApiUrl = "http://localhost:3000/api";  // Development API URL
-
-const apiUrl = process.env.NEXT_PUBLIC_DEV_MODE === '1' && devApiUrl 
-  ? devApiUrl 
-  : defaultApiUrl;
-=======
 const apiUrl = process.env.NEXT_PUBLIC_DEV_MODE === '1' 
   ? "http://localhost:3000/api"  // Development API URL
   : "https://www.aomworld.pro/api";  // Production API URL
->>>>>>> d99291ea2030bbe4507f7938dd0be2f159c0a8a0
 
   
 
