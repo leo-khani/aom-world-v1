8479c5cd1d9314245d9fbff151fc0f35b14d376f
MrNibo99 <153727578+MrNibo99@users.noreply.github.com>
2024-09-21 23:28:41 +0200

    Update sitemap.ts

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,14 +1,15 @@
 import { MetadataRoute } from 'next';
 
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
-  // Fetch top players for the sitemap
-  const topPlayers = await fetchTopPlayers();
+  // Fetch top players for the sitemap from 5 pages
+  const topPlayers = await fetchTopPlayers(20);
 
-  const playerUrls = topPlayers.map((player: { rlUserId: any; }) => ({
+  const playerUrls = topPlayers.map((player: { rlUserId: any; name: string }) => ({
     url: `https://www.aomworld.pro/player/${player.rlUserId}`,
     lastModified: new Date(),
     changeFrequency: 'daily' as const,
     priority: 0.7,
+    title: `${player.name}'s Profile`, // Add title for player pages
   }));
 
   return [
@@ -25,16 +26,26 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
       priority: 0.9,
     },
     {
-        url: 'https://www.aomworld.pro/player/MrNibo',
-        lastModified: new Date(),
-        changeFrequency: 'daily',
-        priority: 0.7,
-      },
+      url: 'https://www.aomworld.pro/gods-statistics',
+      lastModified: new Date(),
+      changeFrequency: 'daily',
+      priority: 0.7,
+    },
+    {
+      url: 'https://www.aomworld.pro/statistics',
+      lastModified: new Date(),
+      changeFrequency: 'daily',
+      priority: 0.7,
+      title: 'AOM World Pro - Statistics', // Add title for statistics page
+    },
     ...playerUrls,
   ];
 }
 
-async function fetchTopPlayers() {
+async function fetchTopPlayers(pages: number) {
+  let allPlayers: any[] = [];
+
+  for (let page = 1; page <= pages; page++) {
     const response = await fetch("https://api.ageofempires.com/api/agemyth/Leaderboard", {
       method: "POST",
       headers: {
@@ -45,17 +56,20 @@ async function fetchTopPlayers() {
         matchType: "1",
         consoleMatchType: 15,
         searchPlayer: "",
-        page: 1,
+        page: page,
         count: 100, 
         sortColumn: "rank",
         sortDirection: "ASC",
       }),
     });
-  
+
     if (!response.ok) {
-      throw new Error("Failed to fetch top players for sitemap");
+      throw new Error(`Failed to fetch top players for sitemap on page ${page}`);
     }
-  
+
     const result = await response.json();
-    return result.items;
+    allPlayers = allPlayers.concat(result.items);
   }
+
+  return allPlayers;
+}
