42619ecb4ae322057d9d42da1a2f207ee43d52d0
MrNibo99 <leokhani99@outlook.com>
2024-09-27 01:19:38 +0200

    Added Contacts, Abouts Page

--- app/(Home)/about/page.tsx
+++ app/(Home)/about/page.tsx
@@ -4,7 +4,7 @@ import React from "react";
 
 export const metadata: Metadata = {
   title: {
-    default: "Contact",
+    default: "About",
     template: "%s | AoM World",
   },
   description:
@@ -22,11 +22,23 @@ export const metadata: Metadata = {
   ],
 };
 
-export default function ContactPage() {
+/**
+ * The About page component.
+ *
+ * This component is responsible for rendering the About page,
+ * which is currently just a placeholder page with a loading
+ * animation.
+ *
+ * @returns {JSX.Element} The rendered page component.
+ */
+export default function About() {
   return (
     <div className="flex flex-col gap-4 justify-center items-center h-screen">
+      {/* A loading animation to indicate that the page is still in development. */}
       <LoadingBear />
-      <h1 className="text-3xl font-bold">Contact Page</h1>
+      {/* A heading to indicate that this is the About page. */}
+      <h1 className="text-3xl font-bold">About</h1>
+      {/* A paragraph to indicate that the page is still being worked on. */}
       <p className="text-lg text-center">Working on it...</p>
     </div>
   );
