da2a46bae74ddb85cef62fc2fa1344caf1d8b3e1
MrNibo99 <leokhani99@outlook.com>
2024-09-26 21:25:26 +0200

    Update page.tsx

--- app/(Home)/contact/page.tsx
+++ app/(Home)/contact/page.tsx
@@ -5,7 +5,7 @@ export default function ContactPage() {
   return (
     <div className="flex flex-col gap-4 justify-center items-center h-screen">
       <LoadingBear />
-      <h1 className="text-3xl font-bold">Wiki</h1>
+      <h1 className="text-3xl font-bold">Contact Page</h1>
       <p className="text-lg text-center">Working on it...</p>
     </div>
   );
